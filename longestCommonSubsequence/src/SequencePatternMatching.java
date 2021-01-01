587b41663c77c2f649a8b44b94b4dab30252648f
Stuti Srivastava <stutisrivastv@gmail.com>
2021-01-02 02:59:23 +0530

    Practice program for - finding if String A is present in String B as a subsequence

---
+++ longestCommonSubsequence/src/SequencePatternMatching.java
@@ -0,0 +1,27 @@
+public class SequencePatternMatching {
+    public static void main(String[] args) {
+        String X = "AXY";
+        String Y = "ABXCDY";
+        System.out.println(spm(X.toCharArray(), Y.toCharArray(), X.length(), Y.length()));
+    }
+
+    private static boolean spm(char[] X, char[] Y, int n, int m) {
+        int[][] t = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            t[i][0] = 0;
+        }
+        for (int j = 0; j <= m; j++) {
+            t[0][j] = 0;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if(X[i-1] == Y[j-1]){
+                    t[i][j] = 1 + t[i-1][j-1];
+                }else {
+                    t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
+                }
+            }
+        }
+        return t[n][m] == n;
+    }
+}
