6740c4cb5b51682bf5c9a3ed30659d52f6487d32
Stuti Srivastava <stutisrivastv@gmail.com>
2021-01-03 14:03:01 +0530

    Practice program for - two sum problem - leet code

---
+++ practice/src/TwoSum.java
@@ -0,0 +1,29 @@
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int length = nums.length;
+        int[] rev = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            rev[i] = target - nums[i];
+        }
+
+        for (int i = 0; i < length; i++) {
+            int targetNum = rev[i];
+            for (int j = 0; j < length; j++) {
+                if (i != j && targetNum == nums[j]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+//        int[] twoSum = new TwoSum().twoSum(new int[]{2, 7, 11, 15}, 9);
+//        int[] twoSum = new TwoSum().twoSum(new int[]{3, 2, 4}, 6);
+        int[] twoSum = new TwoSum().twoSum(new int[]{3, 3}, 6);
+        for (int i : twoSum) {
+            System.out.print(i + " ");
+        }
+    }
+}
