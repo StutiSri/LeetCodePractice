5669e4695c2bb6796f72e3c8d3cd2d0a5b513643
Stuti Srivastava <stutisrivastv@gmail.com>
2021-01-03 14:35:32 +0530

    Practice program for - calculating the longest substring without repeating characters - sliding variable window

--- practice/src/LongestSubstringWithoutRepeatingChars.java
+++ practice/src/LongestSubstringWithoutRepeatingChars.java
@@ -1,53 +1,32 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingChars {
-    Map<String, Integer> t = new HashMap<>();
     public int lengthOfLongestSubstring(String s) {
-        Integer lengthOfString = t.get(s);
-        if(lengthOfString != null){
-            return lengthOfString;
-        }
-        int length = s.length();
-        if (length == 0) {
-            return 0;
-        }
-        if (length == 1) {
-            t.put(s, 1);
-            return 1;
-        }
-        if (stringWithUniqueChars(s)) {
-            t.put(s, length);
-            return length;
-        }
-        int maxLength = Integer.MIN_VALUE;
-        for (int i = 0; i < length - 1; i++) {
-            String leftSubstring = s.substring(0, i + 1);
-            Integer left = t.get(leftSubstring);
-            if(left == null){
-                left = lengthOfLongestSubstring(leftSubstring);
+        int maxLength = 0;
+        int n = s.length();
+        int i = 0;
+        int j = 0;
+        Set<Character> currentSubString = new HashSet<>();
+        while (j < n) {
+            char currentChar = s.charAt(j);
+            if (currentSubString.contains(currentChar)) {
+                while (currentSubString.contains(currentChar)) {
+                    currentSubString.remove(s.charAt(i));
+                    i++;
+                }
             }
-            String rightSubstring = s.substring(i + 1);
-            Integer right = t.get(rightSubstring);
-            if(right == null){
-                right = lengthOfLongestSubstring(rightSubstring);
+            currentSubString.add(currentChar);
+            if (currentSubString.size() > maxLength) {
+                for (Character character : currentSubString) {
+                    System.out.print(character);
+                }
+                System.out.println();
             }
-            int temp = Math.max(left, right);
-            maxLength = Math.max(temp, maxLength);
+            maxLength = Math.max(maxLength, currentSubString.size());
+            j++;
         }
-        t.put(s, maxLength);
         return maxLength;
-    }
-
-    private boolean stringWithUniqueChars(String str) {
-        Map<Character, Boolean> chars = new HashMap<>();
-        for (char character : str.toCharArray()) {
-            if (chars.get(character) != null) {
-                return false;
-            }
-            chars.put(character, true);
-        }
-        return true;
     }
 
     public static void main(String[] args) {
